5644129b55bb75bacbcfda82aa247d19b3527111
Maggie Hu <8508601@isd535.org>
2022-03-15 11:28:20 -0500

    part 3 page 9

---
+++ scripts/formhandler.js
@@ -0,0 +1,33 @@
+(function (window) {
+    'use strict';
+
+    let App = window.App || {};
+    let $ = window.jQuery;
+
+    function FormHandler(selector) {
+      if(!selector) {
+        throw new Error('No selector provided!');
+      }
+
+      //find the "selector" in the DOM using jQuery and assign it to this.formElement
+      this.$formElement = $(selector);
+      if (this.$formElement.length == 0) {
+          throw new Error('Could not find element with selector: ' + selector);
+      }
+    }
+
+    //add an event handler for the submit button
+    FormHandler.prototype.addSubmitHandler = function() {
+        console.log('Setting the submit handler for the form');
+        this.$formElement.on('submit', function(event) {
+            event.preventDefault();
+
+            let data = $(this).serializeArray();
+            console.log(data);
+        });
+    }
+
+    App.FormHandler = FormHandler;
+    window.App = App;
+})(window);
+
