e9336277a15da0af78c7584cf6cb131f58e60b16
Maggie Hu <8508601@isd535.org>
2022-03-17 11:07:00 -0500

    finished part 3

--- scripts/formhandler.js
+++ scripts/formhandler.js
@@ -17,13 +17,22 @@
     }
 
     //add an event handler for the submit button
-    FormHandler.prototype.addSubmitHandler = function() {
+    FormHandler.prototype.addSubmitHandler = function(func) {
         console.log('Setting the submit handler for the form');
         this.$formElement.on('submit', function(event) {
             event.preventDefault();
 
-            let data = $(this).serializeArray();
+            //copy all the info from the form fields into the variable called data
+            let data = {};
+            $(this).serializeArray().forEach(function(item) {
+              data[item.name] = item.value;
+              console.log(item.name + ' is ' + item.value);
+            });
             console.log(data);
+            func(data); //call the function that was passed in on the data from the form
+
+            this.reset(); //resets the form
+            this.elements[0].focus(); //focus on the first field
         });
     }
 
--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,20 @@
 (function (window) {
     'use strict';
 
+    const FORM_SELECTOR = '[data-coffee-order="form"]';
     let App = window.App;
     let Truck = App.Truck;
     let DataStore = App.DataStore;
+    let FormHandler = App.FormHandler;
 
     let myTruck = new Truck('12345', new DataStore());
 
     window.myTruck = myTruck;
+
+    //find the form that is being submitted and create a formHandler object
+    let formHandler = new FormHandler(FORM_SELECTOR);
+    //bind the crateOrder to a specific truck and pass it to addSubmitHandler
+    formHandler.addSubmitHandler(myTruck.createOrder.bind(myTruck));
+    console.log(formHandler);
 
 })(window);
