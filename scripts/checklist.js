19e466fda5ca71b4ed0c81570b0f9a262e0ea5d9
Maggie Hu <8508601@isd535.org>
2022-03-21 11:22:26 -0500

    finished part 4

--- scripts/checklist.js
+++ scripts/checklist.js
@@ -13,10 +13,38 @@
         }
     }
 
+    //when the checkbox is clicked, get the email address from the row
+    //and then call the function (func) that is passed in with the email as a parameter
+    CheckList.prototype.addClickHandler = function (func) {
+        this.$element.on('click', 'input', function (event) {
+            var email = event.target.value;
+            this.removeRow(email);
+            func(email);
+        }.bind(this));
+    };
+
+    //remove a row identified by an email address
+    CheckList.prototype.removeRow = function (email) {
+        this.$element
+            .find('[value="' + email + '"]')
+            .closest('[data-coffee-order="checkbox"]')
+            .remove();
+    }
+
+    //the method that adds a new row to the checklist
+    CheckList.prototype.addRow = function (coffeeOrder) {
+        //remove any existing rows that match the email address
+        this.removeRow(coffeeOrder.emailAddress);
+        //create a new instance of a row, using the coffee order info
+        var rowElement = new Row(coffeeOrder);
+        //add the new row instance's $element property to the checklist
+        this.$element.append(rowElement.$element);
+    };
+
     //each row is one order
     function Row(coffeeOrder) {
         let $div = $('<div></div>', {
-            'data-coffee-order': 'checkbox', 
+            'data-coffee-order': 'checkbox',
             'class': 'checkbox'
         });
         let $label = $('<label></label>');
@@ -25,6 +53,20 @@
             type: 'checkbox',
             value: coffeeOrder.emailAddress
         });
+
+        let description = coffeeOrder.size + ' ';
+        if (coffeeOrder.flavor) {
+            description += coffeeOrder.flavor + ' ';
+        }
+        description += coffeeOrder.coffee + ', ';
+        description += ' (' + coffeeOrder.emailAddress + ')';
+        description += ' [' + coffeeOrder.strength + 'x]';
+
+        $label.append($checkbox);
+        $label.append(description);
+        $div.append($label);
+
+        this.$element = $div;
     }
 
     App.CheckList = CheckList;
